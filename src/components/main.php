b551033153c65eb5f894fe6ae360463b47b22a2f
David Larsson <davidelarsson70@outlook.com>
2024-01-10 18:26:54 +0100

    🔨 Added a main-section for the front page of the website

---
+++ src/components/main.php
@@ -0,0 +1,17 @@
+<section id="intro" class="p-4">
+    <div class="container-lg mt-3">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-md-7 text-center text-md-start">
+                <h2>
+                    <div class="display-2">RiffRealm Guitars</div>
+                    <div class="display-6 text-muted">Learn guitar today!</div>
+                </h2>
+                <p class="text-muted">Get your hands dirty and play your favorite solos</p>
+                <a href="#" class="btn btn-secondary">Contact us!</a>
+            </div>
+            <div class="col-md-5 text-center d-none d-md-block">
+                <img class="img-fluid" width="30%" src="./assets/guitar.png" alt="">
+            </div>
+        </div>
+    </div>
+</section>
