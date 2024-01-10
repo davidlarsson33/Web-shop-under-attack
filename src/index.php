f12f74813bf2ea9c92f626b0241c4e4c8f18de53
David Larsson <davidelarsson70@outlook.com>
2024-01-10 18:27:38 +0100

    🔨 Added a pricing section

--- src/index.php
+++ src/index.php
@@ -12,7 +12,7 @@
 <body>
   <?php include "./components/navbar.php"; ?>
   <?php include "./components/main.php"; ?>
-
+  <?php include "./components/pricing.php"; ?>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
     crossorigin="anonymous"></script>
