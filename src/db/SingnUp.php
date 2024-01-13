babc918485bbfa2979b7aca2828c5ddc37da2d22
David Larsson <davidelarsson70@outlook.com>
2024-01-13 23:45:17 +0100

    ♻️ Removed code that got extracted into the FormValidator

--- src/db/SingnUp.php
+++ src/db/SingnUp.php
@@ -1,35 +1,36 @@
 <?php include "database/DatabaseHandler.php"; ?>
+<?php include "database/DatabaseHandler.php"; ?>
 
 <?php
 class SignUp extends DatabaseHandler
-{
+{   
+    private $formValidator;
+    public function __construct(){
+        $this -> formValidator = new FormValidator();
+    }
+    
 
     //TODO: Close connection to db
-    function signUp($email, $password)
+    function signUp($formData)
     {
-        if ($this->validCredentials($email, $password)) {
-            if (!$this->userExists($email)) {
-                $this->queryInsecure(
-                    "INSERT INTO users (email, password) VALUES ($email, $password)"
-                );
+        $errors = $this->formValidator->validCredentials($formData);
+        if (sizeof($errors) == 0) {
+            // if (!$this->userExists($email)) {
+            //     $this->queryInsecure(
+            //         "INSERT INTO users (email, password) VALUES ($email, $password)"
+            //     );
 
-                header("Location: login.php");
-                exit();
-            } else {
-                echo "User exists!";
-            }
+            //     header("Location: login.php");
+            //     exit();
+            // } else {
+            //     echo "User exists!";
+            // }
 
         } else {
-            echo "Invalid inputs";
+            return array("a"=>12);
         }
     }
 
-    private function validCredentials($email, $password)
-    {
-        return
-            $this->validEmail($email)
-            && $this->validPassword($password);
-    }
 
     private function userExists($email)
     {
@@ -38,51 +39,5 @@ class SignUp extends DatabaseHandler
         return $result->fetchColumn();
     }
 
-    private function validEmail($email)
-    {
-        return !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL);
-    }
-
-    private function validPassword($password)
-    {
-        return
-            !empty($password)
-            && !$this->blackListedPasswords($password)
-            && $this->containsUppercase($password)
-            && $this->containsLowercase($password)
-            && $this->containsNumber($password)
-            && $this->containsSpecialChars($password)
-            && $this->validPasswordLength($password);
-    }
-
-    private function containsUppercase($string)
-    {
-        return preg_match('@[A-Z]@', $string);
-    }
-
-    private function containsLowercase($string)
-    {
-        return preg_match('@[a-z]@', $string);
-    }
-    private function containsNumber($string)
-    {
-        return preg_match('@[0-9]@', $string);
-    }
-
-    private function containsSpecialChars($string)
-    {
-        return preg_match('@[^\w]@', $string);
-    }
-
-    private function validPasswordLength($passwordStr, $required_length = 12)
-    {
-        return strlen($passwordStr) >= $required_length;
-    }
-
-    private function blackListedPasswords($passwordStr)
-    {
-        $blackListedPasswords = array_map('trim', file("blacklist.txt"));
-        return !in_array($passwordStr, $blackListedPasswords);
-    }
 }
 ?>
