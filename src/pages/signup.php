8ff85bf1d90f17b3c8606c90d36035e4f054fe74
David Larsson <davidelarsson70@outlook.com>
2024-01-14 00:13:33 +0100

    🔨 Added a redirect for sign up page. User ger redirected when requested input is provided

--- src/pages/signup.php
+++ src/pages/signup.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-
 <?php
 include "../utilities/FormValidator.php";
 // Server side validation
@@ -20,11 +9,21 @@ if (isset($_POST["submit"])) {
     $errors = $validator->validateFormData($_POST);
 
     if (sizeof($errors) == 0) {
-       // TODO:
+        header('Location: login.php');
+        exit;
     } 
 }
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
 
 <body>
 
