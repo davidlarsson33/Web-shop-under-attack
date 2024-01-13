ee8e449a436af4272ab43687b074e205c1197ff1
David Larsson <davidelarsson70@outlook.com>
2024-01-14 00:32:07 +0100

    ♻️ Refactoring to the SingnUp database handler class

--- src/db/SingnUp.php
+++ src/db/SingnUp.php
@@ -3,36 +3,16 @@
 
 <?php
 class SignUp extends DatabaseHandler
-{   
-    private $formValidator;
-    public function __construct(){
-        $this -> formValidator = new FormValidator();
-    }
-    
-
+{
     //TODO: Close connection to db
-    function signUp($formData)
+    function signUp($email, $password, $name)
     {
-        $errors = $this->formValidator->validCredentials($formData);
-        if (sizeof($errors) == 0) {
-            // if (!$this->userExists($email)) {
-            //     $this->queryInsecure(
-            //         "INSERT INTO users (email, password) VALUES ($email, $password)"
-            //     );
-
-            //     header("Location: login.php");
-            //     exit();
-            // } else {
-            //     echo "User exists!";
-            // }
-
-        } else {
-            return array("a"=>12);
-        }
+        $this->queryInsecure(
+            "INSERT INTO users (email, password) VALUES ($email, $password)"
+        );
     }
 
-
-    private function userExists($email)
+    public function userExists($email)
     {
         $stmt = "SELECT EXISTS(SELECT 1 FROM users WHERE email = $email)";
         $result = $this->queryInsecure($stmt);
