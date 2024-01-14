21b185af086ae1169d6888234d63be89f9fc587b
David Larsson <davidelarsson70@outlook.com>
2024-01-14 17:13:17 +0100

    🔨 Implemented the review functionality

--- src/pages/index.php
+++ src/pages/index.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+include "../db/ReviewDbHandler.php";
+if (isset($_POST["submit"])) {
+  $review = $_POST["text-area"];
+  $db = new ReviewDbHandler();
+  $db->insertReview($_POST);
+}
+
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -10,24 +21,24 @@
 </head>
 
 <body>
+
   <?php include "../components/navbar.php"; ?>
   <?php include "../components/main.php"; ?>
   <?php include "../components/pricing.php"; ?>
-  <?php include "../db/ReviewDbHandler.php"; ?>
 
   <div class="container mt-3" style="min-height:500px">
     <h1>Reviews</h1>
 
     <?php
-      $db = new ReviewDbHandler();
-      $result = $db->fetchReviews();
-      
-      foreach ($result as $row){
-        $name =  $row["name"];
-        $review = $row["review"];
-        $create_at = $row["created_at"];
+    $db = new ReviewDbHandler();
+    $result = $db->fetchReviews();
 
-        echo "
+    foreach ($result as $row) {
+      $name = $row["name"];
+      $review = $row["review"];
+      $create_at = $row["created_at"];
+
+      echo "
           <div class='border border-primary rounded mb-3 mt-3'>
             <div class='p-2'>
               <h5>$name</h5> 
@@ -38,15 +49,16 @@
             </div>
           </div>
         ";
-      }
+    }
     ?>
 
-    <form action="index.php">
+    <form action="index.php" method="POST">
       <p><label for="w3review">Write your review!</label></p>
-      <textarea id="w3review" name="w3review" rows="4" cols="50"></textarea>
+      <textarea id="w3review" rows="4" cols="50" name="text-area"></textarea>
       <br>
-      <input type="submit" value="Submit">
+      <input class="btn rounded bg-primary text-white" name="submit" type="submit" value="Submit">
     </form>
+
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
