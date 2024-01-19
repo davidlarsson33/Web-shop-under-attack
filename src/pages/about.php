92fc55f1e608b3932697fa40f8871d9a93091bcc
David Larsson <davidelarsson70@outlook.com>
2024-01-19 21:09:43 +0100

    🔨 Added an About page

---
+++ src/pages/about.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "../components/navbar.php"; ?>
+
+<div class="container p-5 ">
+    <h2>About</h2>
+
+    <div class="container ps-5">
+        <img src="notes.png" alt="" style="float: right; max-height: 400px; padding-left: 10px;">
+    </div>
+    <p>
+        Meet me, a passionate and skilled guitar virtuoso dedicated to sharing the joy of music through online
+        lessons. With years of experience honing their craft, I have become a trusted guide for aspiring guitarists
+        around the globe.
+    </p>
+    <p>
+        What sets me apart is not just their technical expertise but also their unwavering commitment to making
+        learning enjoyable and accessible. Whether you're a beginner navigating the fretboard for the first time or
+        an
+        intermediate player looking to refine your skills, my lessons cater to all levels.
+    <p>
+        With a teaching style that combines patience, enthusiasm, and a deep understanding of musical theory, I
+        strive to empower students to unlock their full potential on the guitar. Each lesson is thoughtfully curated
+        to
+        blend fundamental techniques with the joy of playing, ensuring that students not only grasp the technical
+        aspects
+        but also develop a genuine love for creating music.
+    </p>
+    <p>
+        As a testament to my impact, countless students have praised the personalized attention and effective
+        teaching methods that have accelerated their musical journey. Through my online platform, aspiring
+        musicians embark on a transformative experience that goes beyond learning chords and scales – it's about
+        cultivating
+        a lifelong passion for playing the guitar. Join my on this musical adventure, and let the strings of
+        creativity resonate in your life.
+    </p>
+
+
+
+
+</div>
+
+<?php include "../components/footer.php"; ?>
+
+</body>
+
+</html>
