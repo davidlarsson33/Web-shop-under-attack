f678bcfaf641d8ba7f426411c13c04b429c65362
David Larsson <davidelarsson70@outlook.com>
2024-01-23 10:11:25 +0100

    ♻️ Extracted routes to a seperate file

--- src/pages/router.php
+++ src/pages/router.php
@@ -3,14 +3,7 @@
 $uri = parse_url($_SERVER["REQUEST_URI"]);
 $requestedPath = $uri["path"];
 
-$routes = [
-    '/' => 'index.php',
-    '/about' => 'about.php',
-    '/signup' => 'signup.php',
-    '/login' => 'login.php',
-    '/reviews' => 'Reviews.php',
-    '/pricing' => 'pricing.php'
-];
+$routes = require "routes.php";
 
 if (array_key_exists($requestedPath, $routes)) {
     require $routes[$requestedPath];
---
+++ src/pages/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    '/' => 'index.php',
+    '/about' => 'about.php',
+    '/signup' => 'signup.php',
+    '/login' => 'login.php',
+    '/reviews' => 'Reviews.php',
+    '/pricing' => 'pricing.php',
+    '/account' => 'account.php',
+    '/logout' => 'logout.php'
+];
+?>
