5a3265b4181b5c1024a286a49db80b2bc2301636
David Larsson <davidelarsson70@outlook.com>
2024-01-24 22:08:42 +0100

    🔨 Added reviews page

---
+++ src/pages/Reviews.php
@@ -0,0 +1,127 @@
+<?php
+session_start();
+
+require_once base_path("src/db/ReviewDbHandler.php");
+require_once base_path("src/utilities/ReviewFormValidator.php");
+
+$dbConfig = require_once base_path("src/db/configurations/dbconfig.php");
+$db = new ReviewDbHandler($dbConfig, 'db-username', 'db-password');
+$errors = [];
+
+if (isset($_POST["submit"])) {
+  $review = $_POST["text-area"];
+  $review = $_POST["nbrOfStars"];
+  $errors = (new ReviewFormValidator())->validateFormData($_POST);
+
+  if (empty($errors)) {
+    $db->insertReview($_POST);
+  }
+
+}
+?>
+
+<?php include base_path("src/partials/navbar.php"); ?>
+
+<style>
+  .stars {
+    display: inline-block;
+    font-size: 30px;
+    cursor: pointer;
+  }
+
+  .stars::before {
+    content: '\2605';
+    color: #ccc;
+  }
+
+  .selected::before {
+    color: orange;
+  }
+</style>
+
+<div class="container mt-3 p-5" style="min-height:500px">
+  <h1>Reviews</h1>
+
+  <?php
+
+  $result = $db->fetchReviews();
+
+  foreach ($result as $row) {
+    $name = $row["name"];
+    $review = $row["review"];
+    $create_at = $row["created_at"];
+    $stars = $row["stars"];
+    ?>
+
+    <div class='border border-black rounded mb-3 mt-3' style='background-color:#DFDFDF; width:50%; max-width:400px;'>
+      <div class='p-2'>
+        <h5>
+          <?= $name ?>
+        </h5>
+        <h6 class='text-muted'>
+          <?= $create_at ?>
+        </h6>
+        <?php for ($x = 0; $x < $stars; $x++) { ?>
+          <img src="/assets/star.png" alt="" width="20px">
+        <?php } ?>
+        <div class='pt-2 pb-3'>
+          <?= $review ?>
+        </div>
+      </div>
+    </div>
+
+  <?php } ?>
+
+  <?php if (isset($_SESSION['user'])) { ?>
+    <form method="POST" id="as">
+      <p><label for="w3review">Write your review!</label></p>
+
+      <div class="stars" onclick="setRating(1)"></div>
+      <div class="stars" onclick="setRating(2)"></div>
+      <div class="stars" onclick="setRating(3)"></div>
+      <div class="stars" onclick="setRating(4)"></div>
+      <div class="stars" onclick="setRating(5)"></div>
+
+      <br>
+
+      <textarea class="mb-2" id="w3review" rows="4" cols="50" name="text-area" required></textarea>
+      <br>
+
+      <input id="nbrOfStars" name="nbrOfStars" type="hidden" value="0">
+      <input name="name" type="hidden" value="<?= $_SESSION['user'] ?>">
+
+      <script>
+        let selectedStars = 0;
+
+        function setRating(stars) {
+          selectedStars = stars;
+
+          document.getElementById('nbrOfStars').value = selectedStars;
+
+          document.querySelectorAll('.stars').forEach((star, index) => {
+            star.classList.remove('selected');
+
+            if (index < selectedStars) {
+              star.classList.add('selected');
+            }
+          });
+        }
+      </script>
+
+      <?php foreach ($errors as $error) { ?>
+        <div class="text-danger">
+          <?= $error ?>
+        </div>
+      <?php } ?>
+
+
+      <input class="btn rounded bg-primary text-white " name="submit" type="submit" value="Submit">
+    </form>
+
+  <?php } else { ?>
+    <h2>You must log in in order to write reviews!</h2>
+  <?php } ?>
+  
+</div>
+
+<?php include base_path("src/partials/footer.php"); ?>
