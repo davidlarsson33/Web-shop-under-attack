b4d8d7905b1f8d70b5a88528139c103dddfd4855
David Larsson <davidelarsson70@outlook.com>
2024-01-25 12:16:24 +0100

    🔨 Added 404 page

---
+++ src/pages/404.php
@@ -0,0 +1,11 @@
+<?php session_start(); ?>
+
+<?php include base_path("src/partials/navbar.php"); ?>
+
+<div class="container p-5 text-center">
+    <h1 class="text-danger">404</h1>
+    <p>Oops! The page you're looking for might be in another parallel universe </p>
+    <a href="/">Go back to home</a>
+</div>
+
+<?php include base_path("src/partials/footer.php"); ?>
