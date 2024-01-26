58f31aa5366935ba570dec2604a8e6219f57c8d1
David Larsson <davidelarsson70@outlook.com>
2024-01-26 20:08:37 +0100

    🔧 Integrated middleware functionality to route handler & routes

--- src/routing/RouteHandler.php
+++ src/routing/RouteHandler.php
@@ -1,37 +1,44 @@
 <?php
+require_once base_path("src/routing/middleware/Middleware.php");
 class RouteHandler
 {
     public $routes = [];
 
     function get($uri, $controller)
     {
-        $this->addRoute($uri, $controller, 'GET');
+        return $this->addRoute($uri, $controller, 'GET');
     }
 
     function post($uri, $controller)
     {
-        $this->addRoute($uri, $controller, 'POST');
+        return $this->addRoute($uri, $controller, 'POST');
     }
 
     function delete($uri, $controller)
     {
-
-        $this->addRoute($uri, $controller, 'DELETE');
+        return $this->addRoute($uri, $controller, 'DELETE');
     }
 
     function put($uri, $controller)
     {
-        $this->addRoute($uri, $controller, 'PUT');
+        return $this->addRoute($uri, $controller, 'PUT');
     }
 
     function patch($uri, $controller)
     {
-        $this->addRoute($uri, $controller, 'PATCH');
+        return $this->addRoute($uri, $controller, 'PATCH');
     }
 
     private function addRoute($uri, $controller, $method)
     {
-        $this->routes[] = compact('uri', 'controller', 'method');
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => $method,
+            'middleware' => null
+        ];
+
+        return $this;
     }
     function route($uri, $method)
     {
@@ -39,6 +46,9 @@ class RouteHandler
         foreach ($this->routes as $route) {
 
             if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+
+                Middleware::resolve($route['middleware']);
+
                 return require $route['controller'];
             }
         }
@@ -49,6 +59,12 @@ class RouteHandler
     private function abort()
     {
         return require base_path("src/views/404.php");
+    }
+
+    public function restrictTo($key)
+    {
+        $this->routes[count($this->routes) - 1]['middleware'] = $key;
+        return $this;
     }
 }
 
--- src/routing/routes.php
+++ src/routing/routes.php
@@ -5,18 +5,18 @@ $controllersDir = "../src/controllers";
 $routeHandler -> get('/', "$controllersDir/index.php");
 $routeHandler -> get('/about', "$controllersDir/about.php");
 $routeHandler -> get('/signup', "$controllersDir/signup.php");
-$routeHandler -> get('/login', "$controllersDir/login.php");
+$routeHandler -> get('/login', "$controllersDir/login.php")->restrictTo("guest");
 $routeHandler -> get('/reviews', "$controllersDir/reviews.php");
 $routeHandler -> get('/pricing', "$controllersDir/pricing.php");
-$routeHandler -> get('/account', "$controllersDir/account.php");
-$routeHandler -> get('/logout', "$controllersDir/logout.php");
+$routeHandler -> get('/account', "$controllersDir/account.php")->restrictTo("authenticated");;
+$routeHandler -> get('/logout', "$controllersDir/logout.php")->restrictTo("authenticated");;
 $routeHandler -> get('/notfound', "$controllersDir/404.php");
 
-$routeHandler -> post('/reviews', "$controllersDir/review/create.php");
-$routeHandler -> post('/signup', "$controllersDir/account/create.php");
-$routeHandler -> post('/login', "$controllersDir/session/login.php");
-$routeHandler -> post('/logout', "$controllersDir/session/logout.php");
+$routeHandler -> post('/reviews', "$controllersDir/review/create.php")->restrictTo("authenticated");;
+$routeHandler -> post('/signup', "$controllersDir/account/create.php")->restrictTo("guest");;
+$routeHandler -> post('/login', "$controllersDir/session/login.php")->restrictTo("guest");;
+$routeHandler -> post('/logout', "$controllersDir/session/logout.php")->restrictTo("guest");;
 
-$routeHandler -> delete('/account', "$controllersDir/account/destroy.php");
+$routeHandler -> delete('/account', "$controllersDir/account/destroy.php")->restrictTo("authenticated");;
 
 ?>
