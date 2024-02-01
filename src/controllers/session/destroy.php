5a2bde8cec2fd14fea9e4c71a41ac4e52345847d
David Larsson <davidelarsson70@outlook.com>
2024-02-01 11:06:45 +0100

    🔧 The session cookie now gets removed when loging out

--- src/controllers/session/destroy.php
+++ src/controllers/session/destroy.php
@@ -1,5 +1,8 @@
 <?php
 session_unset();
 session_destroy();
+$params = session_get_cookie_params();
+setcookie("PHPSESSID", '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
 header("Location: /");
+exit();
 ?>
