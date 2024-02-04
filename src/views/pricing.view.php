b7406619a7298b9873759dc524d883753258641a
David Larsson <davidelarsson70@outlook.com>
2024-02-04 21:53:40 +0100

    🎨 Redesigned the pricing page

--- src/views/pricing.view.php
+++ src/views/pricing.view.php
@@ -1,6 +1,6 @@
 <?php include base_path("src/views/partials/navbar.view.php"); ?>
 
-<section id="pricing" class="bg-light mt-5 pt-3 pb-3">
+<section id="pricing" class=" mt-5 pt-3 pb-3">
     <div class="container-lg">
         <div class="text-center">
             <h2>Pricing Plans</h2>
@@ -9,14 +9,15 @@
 
         <div class="row my-5 g-0 align-items-center justify-content-center">
             <div class="col-8 col-lg-4 col-xl-3">
-                <div class="card border-0">
+                <div class="card border-2">
                     <div class="card-body text-center py-4">
                         <h4 class="card-title">Starter Edition</h4>
                         <p class="lead card-subtitle">Remote lessons only</p>
                         <p class="display-5 my-4 text-primary fw-bold">$10.99 <span class="small">/mo</span></p>
-                        <p class="card-text mx-5 text-muted d-none d-lg-block">Begin your journey with 'Starter
-                            Edition.' Access essential features, kickstart your experience, and enjoy a budget-friendly
-                            introduction to excellence. Start now!</p>
+                        <p class="card-text mx-2 text-muted d-none d-lg-block">
+                            The Starter Edition Kit for guitar lessons offers beginners a solid foundation with a
+                            quality guitar, basic chord charts, a tuner, and introductory instructional materials,
+                            making learning enjoyable and accessible.</p>
                         <a href="#" class="btn btn-outline-primary btn-lg mt-3">
                             Buy Now
                         </a>
@@ -31,9 +32,10 @@
                         <h4 class="card-title">Complete Edition</h4>
                         <p class="lead card-subtitle">Hybrid solution</p>
                         <p class="display-4 my-4 text-primary fw-bold">$15.99 <span class="small">/mo</span></p>
-                        <p class="card-text mx-5 text-muted d-none d-lg-block">Exclusive bundle: 'Complete
-                            Edition'—ultimate value, premium features, unbeatable price. Elevate your experience with
-                            this limited-time offer. Grab yours now!</p>
+                        <p class="card-text mx-2 text-muted d-none d-lg-block">The Complete Edition Kit for guitar
+                            lessons is a comprehensive package featuring a high-quality guitar, professional tuner,
+                            instructional DVDs or online courses, and essential accessories, ensuring a well-rounded
+                            learning experience for players of all levels.</p>
                         <a href="#" class="btn btn-outline-primary btn-lg mt-3">
                             Buy Now
                         </a>
@@ -42,14 +44,15 @@
             </div>
 
             <div class="col-8 col-lg-4 col-xl-3">
-                <div class="card border-0">
+                <div class="card border-2">
                     <div class="card-body text-center py-4">
                         <h4 class="card-title">Ultimate Edition</h4>
-                        <p class="lead card-subtitle">Real life essons in real life</p>
+                        <p class="lead card-subtitle">Real life lessons</p>
                         <p class="display-5 my-4 text-primary fw-bold">$23.99 <span class="small">/mo</span></p>
-                        <p class="card-text mx-5 text-muted d-none d-lg-block">Unleash the extraordinary with 'Ultimate
-                            Edition.' Packed with exclusive content, cutting-edge features, and unmatched value. Upgrade
-                            your experience!</p>
+                        <p class="card-text mx-2 text-muted d-none d-lg-block">The Ultimate Edition Kit elevates guitar
+                            learning with a top-notch acoustic or electric guitar, advanced digital tuner, expert-led
+                            instructional videos, and professional accessories, delivering an unparalleled and enriching
+                            musical experience.</p>
                         <a href="#" class="btn btn-outline-primary btn-lg mt-3">
                             Buy Now
                         </a>
