322e23570c74ca2082af14fc4f8ba3d7afda876f
David Larsson <davidelarsson70@outlook.com>
2024-02-06 14:41:49 +0100

    🐛 Fixed bug which caused the menu to become unresponsive when collapsed

--- src/views/partials/navbar.view.php
+++ src/views/partials/navbar.view.php
@@ -1,14 +1,11 @@
 <?php require "head.view.php" ?>
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/">Guitar lessons</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContentwq" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Guitar lessons</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-        </button>
-
+          </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
 
@@ -42,14 +39,13 @@
                         </li>
                     </form>
 
-
                 <?php } else { ?>
                     <a class="btn btn-primary" aria-current="page" href="login">Sign in</a>
 
                 <?php } ?>
-
-                </li>
+             
             </ul>
         </div>
     </div>
 </nav>
+
